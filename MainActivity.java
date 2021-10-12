9c903084108a173434ec825ea985995cae34ab59
Aayush Sushil Chourasia <89883449+Aayush7517@users.noreply.github.com>
2021-10-12 16:04:34 -0400

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,29 +1,15 @@
-package com.example.fake_call;
+package com.example.ui_fake_video;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-        final MediaPlayer mp = MediaPlayer.create(this,R.raw.call_recording);
-       // mp.start();
-        ImageView i = (ImageView)findViewById(R.id.imageView);
-       i.setBackgroundResource(R.drawable.fakecall);
-       i.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               mp.start();
-           }
-       });
-
     }
 }
