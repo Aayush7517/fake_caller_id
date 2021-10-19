ece2ed36a5b189ab7c07cdfce56b7ceee73e9fe2
Aayush Sushil Chourasia <89883449+Aayush7517@users.noreply.github.com>
2021-10-18 22:16:20 -0400

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,15 +1,35 @@
-package com.example.ui_fake_video;
+package com.example.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button b1,b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+        b1 = findViewById(R.id.button);
+        b2 = findViewById(R.id.button2);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(MainActivity.this,Second.class);
+                startActivity(i);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, third.class);
+                startActivity(i);
+            }
+        });
     }
 }
