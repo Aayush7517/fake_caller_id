0328657d6faca95e52ba9eb98b57b27ead8ab3bd
Aayush Sushil Chourasia <89883449+Aayush7517@users.noreply.github.com>
2021-11-02 14:47:40 -0400

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.calling;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -7,8 +8,10 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,43 +21,65 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
  EditText text;
- Button btn;
+ Button btn1,btn2;
+ private static final int RESULT_PICK_CONTACT = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         text = findViewById(R.id.txtno);
-        btn = findViewById(R.id.btncall);
+        btn1 = findViewById(R.id.btncall);
+        btn2 = findViewById(R.id.cont);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String no = text.getText().toString();
                 Intent i = new Intent(Intent.ACTION_CALL);
-                String number = text.getText().toString();
+                i.setData(Uri.parse("tel:"+no));
+                startActivity(i);
+            }
+        });
 
-                if(number.trim().isEmpty())
-                {
-                    Toast.makeText(MainActivity.this, "Please Enter your Number", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    i.setData(Uri.parse("tel:"+number));
-                }
 
-                if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED)
-                {
-                    Toast.makeText(MainActivity.this, "Please Grant Permission",Toast.LENGTH_SHORT);
-                    requestpermission();
-                }
-                else{
-                    startActivity(i);
-                }
-
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                startActivityForResult(i,RESULT_PICK_CONTACT);
             }
         });
     }
 
-    private void requestpermission(){
-        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE},1);
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+      //  super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case RESULT_PICK_CONTACT:
+                    ContactPicked(data);
+                    break;
+            }
+        } else {
+            Toast.makeText(this, "Failed to pickup", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void ContactPicked(Intent data){
+        Cursor cursor = null;
+
+        try {
+            String PhoneNo = null;
+            Uri uri = data.getData();
+            cursor = getContentResolver().query(uri,null,null,null);
+            cursor.moveToFirst();
+            int PhoneIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+            PhoneNo = cursor.getString(PhoneIndex);
+            text.setText(PhoneNo);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
